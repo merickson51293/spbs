e545c5171314c6bc1288f52e03de1c8d6f132811
Michael.Erickson <merickson51293@gmail.com>
2021-04-21 14:18:21 -0400

    added all pages and worked on menu page

--- SPDapp/urls.py
+++ SPDapp/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('custinfo', views.custinfo)
+    path('custinfo', views.custinfo),
+    path('cookie_select', views.cookie_select),
+    path('my_story', views.my_story),
+    path('gallery', views.gallery),
+    path('menu', views.menu), 
+    path('faq', views.faq)
 ]
--- SPDapp/views.py
+++ SPDapp/views.py
@@ -5,3 +5,18 @@ def index(request):
 
 def custinfo(request):
     return render(request, "cust_info.html")
+
+def cookie_select(request):
+    return render(request, "cookie_select.html")
+
+def my_story(request):
+    return render(request, "my_story.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+def menu(request):
+    return render(request, "menu.html")
+
+def faq(request):
+    return render(request, "FAQ.html")
