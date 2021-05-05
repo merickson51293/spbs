959e83201fedd572ac75659e69fd53d48a3841d6
Michael.Erickson <merickson51293@gmail.com>
2021-05-05 10:12:24 -0400

    changed spd to spbs

--- SPBS/urls.py
+++ SPBS/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("SPDapp.urls"))
+    path('', include("SPBSapp.urls"))
 ]
--- SPDapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpdappConfig(AppConfig):
-    name = 'SPDapp'
